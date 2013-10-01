adf9a5a13c8813d3b19ac301f298e6265c5fefa1
Dmitriy Shabanov <shabanovd@gmail.com>
2013-10-02 01:17:58 +0400

    add unique id for operation in transaction

--- src/org/exist/storage/NativeBroker.java
+++ src/org/exist/storage/NativeBroker.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Observer;
 import java.util.Stack;
 import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -2064,6 +2065,13 @@ public class NativeBroker extends DBBroker {
             throws IOException {
        if (transaction!=null) {
           dir = new File(dir,"txn."+transaction.getId());
+          if (create && !dir.exists()) {
+             if (!dir.mkdir()) {
+                throw new IOException("Cannot make transaction filesystem directory: "+dir);
+             }
+          }
+          //add unique id for operation in transaction
+          dir = new File(dir,"oper."+UUID.randomUUID().toString());
           if (create && !dir.exists()) {
              if (!dir.mkdir()) {
                 throw new IOException("Cannot make transaction filesystem directory: "+dir);
