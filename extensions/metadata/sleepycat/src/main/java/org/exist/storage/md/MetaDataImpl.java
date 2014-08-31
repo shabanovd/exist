a5458ee83f84acfd756f0e01d81cfffba0ff4e3e
Dmitriy Shabanov <shabanovd@gmail.com>
2014-08-31 23:11:32 +0400

    check for duplicates at metadata pairs

--- extensions/metadata/sleepycat/src/main/java/org/exist/storage/md/MetaDataImpl.java
+++ extensions/metadata/sleepycat/src/main/java/org/exist/storage/md/MetaDataImpl.java
@@ -23,6 +23,7 @@ package org.exist.storage.md;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.exist.Database;
@@ -371,12 +372,23 @@ public class MetaDataImpl extends MetaData {
         Metas metas = getMetas(uri);
         if (metas == null)
             return;
+
+        HashSet<String> check = new HashSet<String>();
         
         EntityCursor<MetaImpl> sub = metadata.subIndex(metas.getUUID()).entities();
         try {
-            for (MetaImpl m : sub)
-                listener.metadata(new QName(m.getKey(), MDStorageManager.NAMESPACE_URI, MDStorageManager.PREFIX) , m.getValue());
+            for (MetaImpl m : sub) {
 
+                String key = m.getKey();
+
+                if (check.contains(key)) {
+                    LOG.error("DOUBLE metadata pair "+key+" = "+m.getValue()+" ["+m.getUUID()+"] @ "+uri);
+                    continue;
+                }
+                check.add(key);
+
+                listener.metadata(new QName(m.getKey(), MDStorageManager.NAMESPACE_URI, MDStorageManager.PREFIX), m.getValue());
+            }
         } finally {
             sub.close();
         }
